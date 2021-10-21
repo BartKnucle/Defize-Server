8b7b5370da43332e6f6bad58d19ca0498914b3d9
BartKnucle <sjarde@yahoo.fr>
2021-10-21 11:48:57 +0200

    Add quest_paths

--- server/services/game/index.js
+++ server/services/game/index.js
@@ -1,9 +1,11 @@
 const quests = require('./quests')
 const quest_players = require('./quest_players')
+const quest_paths = require('./quest_paths')
 const steps = require('./steps')
 
 module.exports = function (app) {
   app.configure(quests)
   app.configure(quest_players)
+  app.configure(quest_paths)
   app.configure(steps)
 }
